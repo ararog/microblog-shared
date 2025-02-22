ddc50f9db94aa5cf3ca2901a6226c547124c50a7
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2025-02-21 21:04:21 -0300

    [fix] adding topics constants

---
+++ server/topics.go
@@ -0,0 +1,15 @@
+package server
+
+const (
+	DELETE_PROFILE               = "delete-profile"
+	DELETE_POST                  = "delete-post"
+	DELETE_MEDIA                 = "delete-media"
+	SEND_VERIFICATION_MAIL       = "send-verification-mail"
+	SEND_RESET_PASSWORD_MAIL     = "send-reset-password-mail"
+	SEND_FOLLOW_REQUEST          = "send-follow-request"
+	SEND_UNFOLLOW                = "send-unfollow"
+	SEND_BLOCK                   = "send-block"
+	SEND_UNBLOCK                 = "send-unblock"
+	SEND_FOLLOW_REQUEST_ACCEPTED = "send-follow-request-accepted"
+	SEND_FOLLOW_REQUEST_REJECTED = "send-follow-request-rejected"
+)
